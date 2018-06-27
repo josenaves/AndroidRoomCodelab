acb7b4faea16636af7aad6785457221d4969b359
José Naves <josenaves@gmail.com>
2018-06-27 14:12:57 -0300

    First commit - add Word entity

---
+++ app/src/main/java/com/josenaves/roomwordsample/Word.java
@@ -0,0 +1,37 @@
+package com.josenaves.roomwordsample;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
+
+@Entity(tableName = "word_table")
+public class Word {
+
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "word")
+    private String word;
+
+    public Word(@NonNull String word) {
+        this.word = word;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    @Override
+    public String toString() {
+        return "Word{" +
+                "word='" + word + '\'' +
+                '}';
+    }
+}
+
+
+
+
+
+
+
