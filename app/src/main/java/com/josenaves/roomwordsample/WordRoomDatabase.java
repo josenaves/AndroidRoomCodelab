e7f2ee94298de1ac7bee7169fdf8bcf27cf636e7
José Naves <josenaves@gmail.com>
2018-06-27 18:02:30 -0300

    Add database callback to populate table with words

--- app/src/main/java/com/josenaves/roomwordsample/WordRoomDatabase.java
+++ app/src/main/java/com/josenaves/roomwordsample/WordRoomDatabase.java
@@ -1,10 +1,13 @@
 package com.josenaves.roomwordsample;
 
 
+import android.arch.persistence.db.SupportSQLiteDatabase;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
 import android.content.Context;
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
 
 @Database(entities = { Word.class }, version = 1)
 public abstract class WordRoomDatabase extends RoomDatabase {
@@ -19,10 +22,34 @@ public abstract class WordRoomDatabase extends RoomDatabase {
                     // create database here
                     instance = Room.databaseBuilder(context.getApplicationContext(),
                             WordRoomDatabase.class, "word_database")
+                            .addCallback(callback)
                             .build();
                 }
             }
         }
         return instance;
+    }
+
+    private static RoomDatabase.Callback callback = new RoomDatabase.Callback() {
+        @Override
+        public void onOpen(@NonNull SupportSQLiteDatabase db) {
+            super.onOpen(db);
+            new PopulateDbAsync(instance).execute();
+        }
+    };
+
+    private static class PopulateDbAsync extends AsyncTask<Void, Void, Void> {
+        private final WordDao dao;
+
+        PopulateDbAsync(WordRoomDatabase db) {
+            dao = db.wordDao();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            dao.insert(new Word("Hello"));
+            dao.insert(new Word("World"));
+            return null;
+        }
     }
 }
